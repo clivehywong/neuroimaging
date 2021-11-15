12ad2acfdc555f717e67307e980d1fcf66f03e92
Clive H.Y. Wong <47729357+clivehywong@users.noreply.github.com>
2021-11-15 15:45:38 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Neuroimaging Notes'
+copyright = '2021, Clive Wong'
+author = 'Clive Wong'
 
 release = '0.1'
 version = '0.1.0'
